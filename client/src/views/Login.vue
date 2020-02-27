3b6a946a52c2c150b12dbdc2d532448a3925d91e
Vadim Prokopev <vadimjprokopev@gmail.com>
2020-02-27 18:23:02 -0500

    Lost file

---
+++ client/src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>Please log in or register</h2>
+    <md-field>
+      <label>Username</label>
+      <md-input type="text" :v-model="username"/>
+    </md-field>
+    <md-field>
+      <label>Password</label>
+      <md-input type="password" :v-model="password"/>
+    </md-field>
+    <md-button @click="registerHandler">Register</md-button>
+    <md-button @click="loginHandler">Login</md-button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    }
+  },
+  methods: {
+    ...mapActions(["register", "login"]),
+    registerHandler() {
+      this.register(this.credentials);
+    },
+    loginHandler() {
+      this.login(this.credentials);
+    }
+  },
+  computed: {
+    credentials() {
+      return {
+        username: this.username,
+        password: this.password
+      }
+    }
+  }
+};
+</script>
